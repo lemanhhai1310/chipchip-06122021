152dc111fdbbf02489e3ddd28ffeef024af146b7
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-12-06 13:55:23 +0700

    update

--- login.php
+++ login.php
@@ -14,15 +14,33 @@
                                 <h1 class="uk-h1 login__title">Đăng nhập</h1>
                             </div>
                             <div class="uk-width-1-1 login__column">
-                                <label class="uk-form-label" for="form-stacked-text">Tài khoản</label>
+                                <label class="uk-form-label login__label" for="form-stacked-text">Tài khoản</label>
                                 <div class="uk-form-controls">
-                                    <input class="uk-input" id="form-stacked-text" type="text" placeholder="Some text...">
+                                    <input class="uk-input login__input uk-border-pill" id="form-stacked-text" type="text" placeholder="Nhập tài khoản...">
                                 </div>
                             </div>
                             <div class="uk-width-1-1 login__column">
-                                <label class="uk-form-label" for="form-stacked-text">Tài khoản</label>
-                                <div class="uk-form-controls">
-                                    <input class="uk-input" id="form-stacked-text" type="text" placeholder="Some text...">
+                                <div class="login__item16">
+                                    <label class="uk-form-label login__label" for="form-stacked-text">Mật khẩu</label>
+                                    <div class="uk-form-controls">
+                                        <input class="uk-input login__input uk-border-pill" id="form-stacked-text" type="text" placeholder="Nhập mật khẩu...">
+                                    </div>
+                                </div>
+                                <div class="login__item16">
+                                    <div class="uk-grid-small uk-child-width-auto uk-grid">
+                                        <label><input class="uk-checkbox login__checkbox" type="checkbox" checked> <span class="login__txt">Hiện mật khẩu</span></label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="uk-width-1-1 login__column">
+                                <button type="button" class="login__btn uk-button uk-button-primary uk-width-1-1 uk-border-pill"><span>Đăng nhập</span></button>
+                            </div>
+                            <div class="uk-width-1-1 login__column uk-text-center">
+                                <div class="login__item16">
+                                    <span class="login__txt">Chưa có tài khoản? <a href="">Đăng ký ngay</a></span>
+                                </div>
+                                <div class="login__item16">
+                                    <a href="" class="login__forgot">Quên mật khẩu?</a>
                                 </div>
                             </div>
                         </div>

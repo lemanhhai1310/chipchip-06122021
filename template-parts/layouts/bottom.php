bfb1c3d3fb7eacddf62aba841ccbfe3f9c2edb79
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-12-14 21:56:08 +0700

    update

--- template-parts/layouts/bottom.php
+++ template-parts/layouts/bottom.php
@@ -6,10 +6,10 @@
                     <a onclick="dodajAktywne(this)" href="#offcanvas-flip-chat" uk-toggle class="bottom__icon bottom__icon--chat"></a>
                 </div>
                 <div class="uk-navbar-item">
-                    <a onclick="dodajAktywne(this)" href="javascript:void(0)" class="bottom__icon bottom__icon--help"></a>
+                    <a onclick="dodajAktywne(this)" href="#offcanvas-flip-help" uk-toggle class="bottom__icon bottom__icon--help"></a>
                 </div>
                 <div class="uk-navbar-item">
-                    <a onclick="dodajAktywne(this)" href="javascript:void(0)" class="bottom__icon bottom__icon--image"></a>
+                    <a onclick="dodajAktywne(this)" href="javascript:void(0)" uk-toggle class="bottom__icon bottom__icon--image"></a>
                 </div>
             </div>
         </nav>
@@ -28,6 +28,7 @@
         elem.classList.add('uk-active');
     }
 </script>
+<!--Chat-->
 <div id="offcanvas-flip-chat" class="bottom__offcanvas" uk-offcanvas="flip: true; overlay: true">
     <button class="uk-offcanvas-close bottom__offcanvas__close" type="button" uk-close></button>
     <div class="uk-offcanvas-bar bottom__offcanvas__bar uk-flex uk-flex-column">
@@ -116,3 +117,64 @@
 
     </div>
 </div>
+<!--/Chat-->
+
+<!--Trợ giúp-->
+<div id="offcanvas-flip-help" class="bottom__offcanvas" uk-offcanvas="flip: true; overlay: true">
+    <button class="uk-offcanvas-close bottom__offcanvas__close" type="button" uk-close></button>
+    <div class="uk-offcanvas-bar bottom__offcanvas__bar uk-flex uk-flex-column">
+        <div class="bottom__offcanvas__header bottom__offcanvas__item">
+            <h3 class="uk-h3 bottom__offcanvas__title">Trợ giúp</h3>
+        </div>
+        <div class="bottom__offcanvas__body bottom__offcanvas__item uk-flex-auto uk-overflow-auto">
+            <div class="login__item16">
+                <div class="home__item24">
+                    <div class="home__item12">
+                        <div class="bottom__offcanvas__help__box1">Cách 1: Tải lại lớp học</div>
+                    </div>
+                    <div class="home__item12">
+                        <button type="button" class="home__panel__btn home__panel__btn__video home__panel__btn__refresh uk-button uk-button-default uk-border-pill">
+                            <span class="home__panel__btn__txt">Tải lại lớp</span>
+                        </button>
+                    </div>
+                </div>
+                <div class="home__item24">
+                    <div class="home__item12">
+                        <div class="bottom__offcanvas__help__box1">Cách 2: Đưa thiết bị lại gần nơi có sóng wifi mạnh</div>
+                    </div>
+                </div>
+                <div class="home__item24">
+                    <div class="home__item12">
+                        <div class="bottom__offcanvas__help__box1">Cách 3: Đổi sang kết nối Internet mạnh hơn</div>
+                    </div>
+                    <div class="home__item12">
+                        <div class="bottom__offcanvas__help__txt">Nếu bạn đã thử các cách trên mà vẫn không khắc phục được, hãy gửi yêu cầu cho Admin Chip Chip để được hỗ trợ.</div>
+                    </div>
+                </div>
+            </div>
+            <div class="login__item16">
+                <div class="home__item12">
+                    <div class="uk-width-1-1" uk-form-custom="target: > * > span:first-child">
+                        <select>
+                            <option value="">Bạn đang gặp vấn đề gì?</option>
+                            <option value="1">Option 01</option>
+                            <option value="2">Option 02</option>
+                            <option value="3">Option 03</option>
+                            <option value="4">Option 04</option>
+                        </select>
+                        <button class="bottom__offcanvas__help__btnSelect uk-button uk-button-default uk-width-1-1" type="button" tabindex="-1">
+                            <span></span>
+                            <span class="uk-position-center-right" uk-icon="icon: chevron-down"></span>
+                        </button>
+                    </div>
+                </div>
+                <div class="home__item12">
+                    <button type="button" class="home__panel__btn home__panel__btn__send uk-width-1-1 uk-button uk-button-default uk-border-pill">
+                        <span class="home__panel__btn__txt">Gửi yêu cầu trợ giúp</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/Trợ giúp-->
